08e2b52464f031594b16c774f6bbe4e6309adf52
genkinaoko <surtaka944@gmail.com>
2021-11-17 10:19:19 +0900

    air

--- Python/lambda.py
+++ Python/lambda.py
@@ -18,3 +18,17 @@ lis = [1,2,3,4]
 print(list(map(lambda x:x**2, lis)))
 # [1, 4, 9, 16]
 
+#ディクショナリのリスト
+employees = [
+    {"name": "John", "age": 21},
+    {"name": "Kevin", "age": 45},
+    {"name": "Amy", "age": 21},
+    {"name": "Ryan", "age": 30}
+]
+result = filter(lambda x: x["age"] > 25, employees)
+print(list(result))
+# [{'name': 'Kevin', 'age': 45}, {'name': 'Ryan', 'age': 30}]
+
+result = map(lambda x: x["name"], (filter(lambda x: x["age"] > 25, employees)))
+print(list(result))
+# ['Kevin', 'Ryan']
