f9407f581f89b53d11b55d36732bb9e44f46546c
genkinaoko <surtaka944@gmail.com>
2021-11-18 19:21:07 +0900

    air

---
+++ C/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("たかさきげんき\n");
+    return 0;
+}
---
+++ C/hello.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    cout << "高崎元希" << endl;
+    return 0;
+}
